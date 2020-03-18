92f924e3a6399dfabd54473d38af6349a683dcb6
Felix Fernandez <fernandezfelix460.gmail.com>
2020-03-18 09:57:47 -0400

    Updates

---
+++ precache-manifest.98f318001054625bf819bd4bbc926053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254df438cd45bc7c5c83ff66745d06c5",
+    "url": "/mockpus/index.html"
+  },
+  {
+    "revision": "73d22f51507650472153",
+    "url": "/mockpus/static/css/main.db964b18.chunk.css"
+  },
+  {
+    "revision": "d00bea034665388930cf",
+    "url": "/mockpus/static/js/2.d1cc3729.chunk.js"
+  },
+  {
+    "revision": "989ac8b04042782519a29910086adb47",
+    "url": "/mockpus/static/js/2.d1cc3729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d22f51507650472153",
+    "url": "/mockpus/static/js/main.a566eda8.chunk.js"
+  },
+  {
+    "revision": "3c50f99ae3f65aa328c7",
+    "url": "/mockpus/static/js/runtime-main.3664298a.js"
+  }
+]);
